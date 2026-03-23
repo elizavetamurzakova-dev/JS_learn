38e816106a52eb3cf92bb24a666419ac5b7269ad
manager <elizaveta.murzakova@intelsib-team.com>
2026-03-23 18:56:48 +0700

    begin exercise 1

---
+++ exercise1/exercise-1.js
@@ -0,0 +1,8 @@
+class Ship{
+    constructor(name_ship, len_ship, hits, startPosition){
+        this.name_ship = name_ship;
+        this.len_ship = len_ship;
+        this.hits = hits;
+        this.len_ship = len_ship;
+    }
+}
