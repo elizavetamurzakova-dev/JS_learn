b30d9bddc1be315e3aae26447d878cd1e1c0a1a6
manager <elizaveta.murzakova@intelsib-team.com>
2026-03-25 17:46:34 +0700

    finish task3

---
+++ exercise3/exercise-1.js
@@ -0,0 +1,272 @@
+class Ship{
+    constructor(name, len, vertical){
+        this._name = name;
+        this._len = len;
+        this._hits = new Array(len).fill(false);
+        this._vertical = vertical;
+        this._startvertical = {x: 0, y:0};
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get len() {
+        return this._len;
+    }
+
+    get hits() {
+        return this._hits;
+    }
+
+    get vertical() {
+        return this._vertical;
+    }
+
+    get startvertical() {
+        return this._startvertical;
+    }
+
+    get position_x() {
+        return this._startvertical.x;
+    }
+
+    get position_y() {
+        return this._startvertical.y;
+    }
+
+
+
+    set name(value) {
+        this._name=value;
+    }
+
+    set len(value) {
+        this._len=value;
+    }
+
+    set hits(value) {
+        this._hits=value;
+    }
+
+    set vertical(value) {
+        this._vertical=value;
+    }
+
+    set startvertical(value) {
+        this._startvertical=value;
+    }
+
+    set position_x(value) {
+        this._startvertical.x=value;
+    }
+
+    set position_y(value) {
+        this._startvertical.y=value;
+    }
+
+    hit(index){
+        if(index < this._len){
+            this._hits[index] = true
+        }
+        else{
+            console.log("Мимо");
+        }
+    }
+
+    isSunk() {
+        if(!this._hits.includes(false)) return true
+        else return false
+    }
+}
+
+class Board {
+    constructor(size){
+        this._size = size;
+        this._grid = Array.from({length: size}, () => new Array(size).fill(null));
+        this._ships = [];
+    }
+
+    get size(){
+        return this._size;
+    }
+
+    get grid(){
+        return this._grid;
+    }
+
+    get ships(){
+        return this._ships;
+    }
+
+
+    set size(value){
+        this._size = value;
+    }
+
+    set grid(value){
+        this._grid = value;
+    }
+
+    set ships(value){
+        this._ships = value;
+    }
+
+    placeShip(ship, x, y) {
+        if (ship.vertical == 0 && (x + ship.len) <= this._size) {
+            for (let i = 0; i < ship.len; i++) {
+                if (this._grid[y][x + i] !== null) return console.log("Мест нет");  
+            }
+            for (let i = 0; i < ship.len; i++) {
+                this._grid[y][x + i] = ship;  
+            }
+            this._ships.push(ship);
+            ship.position_x = x;
+            ship.position_y = y;  
+        } else if (ship.vertical == 1 && (y + ship.len) <= this._size) {
+            for (let i = 0; i < ship.len; i++) {
+                if (this._grid[y + i][x] !== null) return console.log("Мест нет");  
+            }
+            for (let i = 0; i < ship.len; i++) {
+                this._grid[y + i][x] = ship;
+            }
+            this._ships.push(ship);
+            ship.position_x = x; 
+            ship.position_y = y;
+        } else {
+            console.log("Корабль не помещается");
+        }
+    }
+
+    findAvailableCells() {
+        const availableCells = [];
+
+        for (let y = 0; y < this.size; y++) {
+            for (let x = 0; x < this.size; x++) {
+                if (this._grid[y][x] === null) {
+                    availableCells.push({ x, y });
+                }
+            }
+        }
+        return availableCells;
+    }
+
+    receiveAttack(x, y) {
+        const ship = this._grid[y][x];
+
+        if (ship !== null) {
+            if (ship.vertical === 0) {
+                let index = x - ship.position_x;
+                ship.hit(index);
+            } else if (ship.vertical === 1) {
+                let index = y - ship.position_y;
+                ship.hit(index);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    display() {
+        for (let y = 0; y < this._size; y++) {
+            let row = '';
+            for (let x = 0; x < this._size; x++) { 
+                if (this._grid[y][x] === null) {
+                    row += 'O';
+                } else {
+                    const ship = this._grid[y][x];
+                    let isHit = false;
+                
+                    if (ship.vertical === 0) {
+                        let index = x - ship.position_x;
+                        if (ship.hits[index] === true) {
+                            isHit = true;
+                        }
+                    } else if (ship.vertical === 1) {
+                        let index = y - ship.position_y;
+                        if (ship.hits[index] === true) {
+                            isHit = true;
+                        }
+                    }
+                    
+                    row += isHit ? 'X' : 'S';
+                }
+            }
+            console.log(row);
+        }
+    }
+}
+
+class Player{
+    constructor(name, boardSize){
+        this._name = name;
+        this._boardSize = boardSize;
+        this._board = new Board(boardSize);
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get boardSize() {
+        return this._boardSize;
+    }
+
+    get board() {
+        return this._board;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    set boardSize(value) {
+        this._boardSize = value;
+    }
+
+    set board(value) {
+        this._board = value;
+    }
+
+    placeShips(shipName, length, isVertical, startPosition){
+        let ship = new Ship(shipName, length, isVertical);
+        this._board.placeShip(ship, startPosition.x, startPosition.y);
+    }
+
+    takeTurn(opponent) {
+        let xInput = prompt("Координата Х:");
+        if (xInput === null) return null;
+
+        let x = +xInput;
+        if (isNaN(x) || !Number.isInteger(x)) {
+            alert("Координата X должна быть целым числом");
+            return null;
+        }
+
+        let yInput = prompt("Координата Y:");
+        if (yInput === null) return null;
+
+            let y = +yInput;
+            if (isNaN(y) || !Number.isInteger(y)) {
+                alert("Координата Y должна быть целым числом");
+            return null;
+        }
+        if (x < 0 || x >= opponent.boardSize) {
+            alert(`Координата X должна быть от 0 до ${opponent.boardSize - 1}`);
+            return null;
+        }
+        if (y < 0 || y >= opponent.boardSize) {
+            alert(`Координата Y должна быть от 0 до ${opponent.boardSize - 1}`);
+            return null;
+        }
+
+        return { x: x, y: y, opponent: opponent }
+    }
+}
+
+let input = prompt('Введите имя игрока и размер поля через пробел:');
+
+let arr = input.split(' ');
+let player = new Player(arr[0], parseInt(arr[1]));
+
+console.log(player.name, player.boardSize);
